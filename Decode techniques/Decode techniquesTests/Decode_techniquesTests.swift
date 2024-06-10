65d4d65f1ea330a32657e581c298147a802e3378
Yuri Cavalcanti <yuri.cavalcanti2013@gmail.com>
2024-06-10 17:10:23 -0300

    add spm quick and nimble

--- Decode techniques/Decode techniquesTests/Decode_techniquesTests.swift
+++ Decode techniques/Decode techniquesTests/Decode_techniquesTests.swift
@@ -1,36 +1,12 @@
-//
-//  Decode_techniquesTests.swift
-//  Decode techniquesTests
-//
-//  Created by Yuri Cavalcanti on 10/06/24.
-//
+import UIKit
+import Quick
+import Nimble
 
-import XCTest
 @testable import Decode_techniques
 
-final class Decode_techniquesTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class Decode_techniquesTests: QuickSpec {
+    // Nova versao o spec fica assim: override class func spec()
+    override func spec() {
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

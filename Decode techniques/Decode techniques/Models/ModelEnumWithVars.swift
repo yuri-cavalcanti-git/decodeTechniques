ceca8f38f651603c0767ea8a0c45fe358ef84b89
Yuri Cavalcanti <yuri.cavalcanti2013@gmail.com>
2024-06-13 16:23:55 -0300

    add ModelEnumWithVars

--- Decode techniques/Decode techniques/Models/ModelEnumWithVars.swift
+++ Decode techniques/Decode techniques/Models/ModelEnumWithVars.swift
@@ -1,8 +1,34 @@
-//
-//  ModelEnumWithVars.swift
-//  Decode techniques
-//
-//  Created by Yuri Cavalcanti on 13/06/24.
-//
-
 import Foundation
+
+/*
+ Tecnica 1: De acordo com o recebido no id, o service {q nao vem de back, variavel auxiliar} recebe um case, caso nao esteja mapeado, recebe o case other
+ Tecnica 2: O enum retorna uma variavel computada de acordo com cada case. Muito util para popular um viewModel devolvendo labels, icons, colors etc para cada case. Desde ser uma computed var, poderia ser uma func.
+*/
+
+struct ModelEnumWithVars: Equatable, Decodable {
+    let id: String
+    var service: Service { Service(id: id) }
+}
+
+extension ModelEnumWithVars {
+    enum Service: String {
+        case gas = "123"
+        case energi = "456"
+        case other
+        
+        init(id: String) {
+            self = Service(rawValue: id) ?? .other
+        }
+        
+        var label: String {
+            switch self {
+            case .gas:
+                return "O serviço é gás"
+            case .energi:
+                return "O serviço é energia"
+            case .other:
+                return "Outro serviço"
+            }
+        }
+    }
+}
